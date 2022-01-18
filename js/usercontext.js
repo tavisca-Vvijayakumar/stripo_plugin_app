7f1e801a863f07275c64e3caa5f06d7c3df72fc9
tavisca-Vvijayakumar <89015369+tavisca-Vvijayakumar@users.noreply.github.com>
2022-01-18 19:09:22 +0530

    2nd commit

--- js/usercontext.js
+++ js/usercontext.js
@@ -1,12 +1,16 @@
-var usercontext = {};
 
+var usercontext = {};
 var userContextConfiguration = {
     /*
     This method is use to load configuration from the content stack in stripo plugin app.
     * @param: userContextObect.
     */
     loadUserContext: async function (userContextObject) {
-        usercontext = userContextObject;
+        console.log(userContextObject)
+        var bytes = CryptoJS.AES.decrypt(userContextObject, 'secret key 123');
+        var decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8)); 
+        console.log(decryptedData); // [{id: 1}, {id: 2}]
+        usercontext = decryptedData;
         return "Ok";
     }
 }
