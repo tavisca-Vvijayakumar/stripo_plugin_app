5c24030e31baeed076849b45a2d99c07573601ff
tavisca-Vvijayakumar <89015369+tavisca-Vvijayakumar@users.noreply.github.com>
2022-01-21 11:26:47 +0530

    Update utility.js

--- js/utility.js
+++ js/utility.js
@@ -101,7 +101,7 @@ var EMAILUtility = {
     getContentStackRequestHeader: function () {
         var header = {
             'api_key': usercontext.apiKey,
-            'authorization': usercontext.authToken,
+            'authtoken': usercontext.authToken,
             'Content-Type': CONTENT_TYPE,
         };
 
